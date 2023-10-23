95de85eeff36d1fa6eaac97ece5c051d374b5e3d
Samson Rozansky <66850179+samson-rozansky@users.noreply.github.com>
2023-10-23 14:11:39 -0400

    Update 5CardDrawTest.py

--- Unit-TestsREALFINALFINAL/5CardDrawTest.py
+++ Unit-TestsREALFINALFINAL/5CardDrawTest.py
@@ -93,7 +93,7 @@ def pontoon():
                         newCard = deck.pop()
                         print(f"{newCard.rank} of {newCard.suit}")
                         player.add_card(newCard)
-                        break
+                        
                     else:
                         print("Invalid option for doubling down.")
                 elif choice == 's':
